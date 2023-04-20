c3fae1a254e2b9cd42580d2a5ef6b6cff37b0b63
anurishramkhelawan <22001344@student.hhs.nl>
2023-04-20 12:58:07 +0200

    Multiply test aangemaakt.

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -2,8 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+    JavaFXApp javaFXApp = new JavaFXApp();
+
     @Test
     void aDummyTest() {
-        assertEquals(0,0);
+        assertEquals(0, 0);
+    }
+
+    @Test
+    void multiplyTest() {
+        assertEquals(25, javaFXApp.computeMultiply(5, 5));
+
     }
 }
