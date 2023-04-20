a35f5bed6bdec816e932e0c9908c038ec480c3f9
Jay Gerners <20127235@student.hhs.nl>
2023-04-20 12:57:57 +0200

    addition gefixt

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -2,8 +2,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+    JavaFXApp javaFXApp = new JavaFXApp();
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+    @Test
+    void additionTest(){
+        assertEquals(5,javaFXApp.computeAdd(2,3));
     }
 }
